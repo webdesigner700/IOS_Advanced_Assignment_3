348abeef72cf818c54366f498356deeeaa101026
Vinay B <vinay.bayyapunedi@gmail.com>
2023-09-26 22:47:13 +1000

    Have a version of Network Layer and External API code in ContentView.swift. Have to figure out how to make it work

--- IOS_Advanced_Assignment_3/ContentView.swift
+++ IOS_Advanced_Assignment_3/ContentView.swift
@@ -6,16 +6,86 @@
 //
 
 import SwiftUI
+import Foundation
+
+struct Dam: Hashable, Codable {
+    
+    let dam_id: Int
+    let dam_name: String
+}
+
+class ViewModel: ObservableObject {
+    
+    @Published var dams: [Dam] = []
+    
+    func fetch() {
+        
+        let headers = [
+          "authorization": "Basic YVl0b3lqUDJyY3ZhOFpkcno5dXg5R0FqVUpDSDNHMEc6OUdhNFNnVDVQakxBZE9OSg==",
+          "apikey": "aYtoyjP2rcva8Zdrz9ux9GAjUJCH3G0G"
+        ]
+
+        let request = NSMutableURLRequest(url: NSURL(string: "https://api.onegov.nsw.gov.au/waternsw-waterinsights/v1/dams")! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if let error = error {
+                print("Error: \(error)")
+            }
+            else if let httpResponse = response as? HTTPURLResponse {
+                
+                if httpResponse.statusCode == 200 {
+                    
+                    if let responseData = data {
+                        // Decode the JSON data into Swift UI objects
+                        
+                        do {
+                            
+                            let dams = try JSONDecoder().decode([Dam].self, from: responseData)
+                            DispatchQueue.main.async {
+                                self.dams = dams
+                            }
+                        }
+                        catch {
+                            
+                        }
+                    }
+                    else {
+                        print("HTTP Response Code: \(httpResponse.statusCode)")
+                    }
+                }
+            }
+        })
+
+        dataTask.resume()
+    }
+}
 
 struct ContentView: View {
+    
+    @StateObject var viewModel = ViewModel()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            List {
+                ForEach(viewModel.dams, id: \.self) { dam in
+                    
+                    HStack {
+                        Text(dam.dam_name)
+                            .bold()
+                    }
+                }
+            }
         }
-        .padding()
+        .navigationTitle("Dams")
+        .onAppear {
+            viewModel.fetch()
+        }
     }
 }
 
--- IOS_Advanced_Assignment_3/Model/DamData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  DamData.swift
-//  IOS_Advanced_Assignment_3
-//
-//  Created by vinay bayyapunedi on 26/09/23.
-//
-
-import Foundation
